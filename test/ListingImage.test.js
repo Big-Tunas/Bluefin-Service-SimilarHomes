1b898c159bef12e9cce587ca8b32e8bafafa910b
Kevin Ong <kevinyaohuiong@gmail.com>
2020-01-24 09:55:10 -0800

    found image sources, added to test suite

---
+++ test/ListingImage.test.js
@@ -0,0 +1,10 @@
+test("Image Source", () => {
+  let listOfImages = [
+    'https://media.architecturaldigest.com/photos/5601b15b5a2d8a2712e8979b/master/w_1600%2Cc_limit/uk-modern-houses-book03.jpg',
+    'https://freshome.com/wp-content/uploads/2018/09/contemporary-exterior.jpg',
+    'https://i.pinimg.com/originals/2b/fb/6b/2bfb6b646097abbc26d218b78370c5c9.jpg',
+    'https://i1.wp.com/www10.aeccafe.com/blogs/arch-showcase/files/2018/09/1.1-min.jpg?w=1000&ssl=1',
+    'https://www.cupapizarras.com/wp-content/uploads/2018/09/casa-pasiva-contemporanea-asturias.jpg'
+  ]
+
+});
