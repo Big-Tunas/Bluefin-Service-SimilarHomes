4b23ac15fe81cf07b5b81e07521a41d0ae31690c
Kevin Ong <kevinyaohuiong@gmail.com>
2020-01-27 12:19:58 -0800

    update with initial testing files

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
+// const aws = require('./aws.js');
 const express = require('express');
 const mainApp = express();
 const mainPort = 4004;
 
 mainApp.use(express.static('public'));
 mainApp.use(express.json());
-mainApp.use(express.urlencoded({extneded: true}));
+mainApp.use(express.urlencoded({extended: true}));
 
 
 mainApp.get('/image', (req, res) => {
--- test/ListingImage.test.js
+++ test/ListingImage.test.js
@@ -1,11 +1,24 @@
-test("Image Source", () => {
-  let listOfImages = [
-    'https://media.architecturaldigest.com/photos/5601b15b5a2d8a2712e8979b/master/w_1600%2Cc_limit/uk-modern-houses-book03.jpg',
-    'https://freshome.com/wp-content/uploads/2018/09/contemporary-exterior.jpg',
-    'https://i.pinimg.com/originals/2b/fb/6b/2bfb6b646097abbc26d218b78370c5c9.jpg',
-    'https://i1.wp.com/www10.aeccafe.com/blogs/arch-showcase/files/2018/09/1.1-min.jpg?w=1000&ssl=1',
-    'https://www.cupapizarras.com/wp-content/uploads/2018/09/casa-pasiva-contemporanea-asturias.jpg'
-  ];
+import React from 'react';
+import { shallow, render, mount } from 'enzyme';
 
-  
+import ListingImage from '../client/src/components/ListingImage.jsx';
+
+// describe('basic test functionality', () => {
+//   it('should pass for a simple test', () => {
+//     expect(true).toBeTruthy();
+//   });
+// });
+
+describe('ListingImage', () => {
+  let source = "https://freshome.com/wp-content/uploads/2018/09/contemporary-exterior.jpg";
+
+  it('renders with an image tag', () => {
+    let wrapper = mount(<ListingImage />);
+    expect(wrapper.find('img')).toBeDefined();
+  });
+
+  it('has a hard-coded source for the image tag', () => {
+    let wrapper = mount(<ListingImage />);
+    expect(wrapper.find('img').prop('src')).toBe(source);
+  });
 });
