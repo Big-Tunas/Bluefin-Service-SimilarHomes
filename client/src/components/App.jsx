888b4a4b7211f3fad40a66ce8cbc9f8310f4facb
Kevin Ong <kevinyaohuiong@gmail.com>
2020-01-27 21:02:47 -0800

    successful api call to db with hard-coded images

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,27 +3,50 @@ import ListingImage from './ListingImage.jsx';
 import styled from 'styled-components';
 import axios from 'axios';
 
-const NearbyListingsContainer = (props) => {
-
-  axios.get(
-    'http://localhost:4004/image',
-    {city: "San Francisco"},
-    (err, image) => {
-      if (err) {
-        console.log(err);
-      } else {
-        return (
-          <ListingImage src="https://freshome.com/wp-content/uploads/2018/09/contemporary-exterior.jpg" alt="4633 Gaviota Court, Bonita, CA 91902" />
-        );
-      }
+class NearbyListingsContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listings: []
     }
-  );
+  }
 
-  return (
-    <h2>Nearby Similar Listings</h2>
-    <ListingImage src="https://freshome.com/wp-content/uploads/2018/09/contemporary-exterior.jpg" />
-    
-  );
+  getListings() {
+    axios.get('http://localhost:4004/image')
+      .then(listings => {
+          this.setState({
+            listings: listings.data
+          });
+      });
+  }
+
+  componentDidMount() {
+    if (!this.state.listings.length) {
+      this.getListings();
+    }
+  }
+
+
+  render() {
+    if (!this.state.listings.length) {
+      return (
+        <div>
+          <ListingImage src="https://freshome.com/wp-content/uploads/2018/09/contemporary-exterior.jpg" />
+        </div>
+      );
+    } else {
+      let homes = this.state.listings;
+      let images = homes.map(listing => {
+        return (<ListingImage src={listing.imageUrl} />);
+      });
+      return (
+        <div>
+          {images}
+        </div>
+      );
+    }
+
+  }
 };
 
 export default NearbyListingsContainer;
